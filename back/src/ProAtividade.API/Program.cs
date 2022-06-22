a081de7dbbe9e698a8db71468228cfedfe21ccd3
amarildovictor <amarildovictor@gmail.com>
2022-06-22 17:06:52 +0100

    Seção 07 - Pequenos ajuste .NET

--- back/src/ProAtividade.API/Program.cs
+++ back/src/ProAtividade.API/Program.cs
@@ -1,5 +1,6 @@
 using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using ProAtividade.Data.Context;
 using ProAtividade.Data.Repositories;
 using ProAtividade.Domain.Interfaces.Repositories;
@@ -25,7 +26,10 @@ builder.Services.AddControllers()
     
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo {Title = "ProAtividade.API", Version = "v1"});
+});
 builder.Services.AddCors();
 
 var app = builder.Build();
@@ -33,8 +37,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProAtividade.API v1"));
 }
 
 app.UseHttpsRedirection();
